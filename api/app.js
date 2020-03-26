b723d719303b120c2ddfa49106e95f683d32b2c1
Santiago <santiagomanuelrodriguez@gmail.com>
2020-03-26 20:57:12 -0300

    serve public client

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload')
+const path = require('path');
 const { notFoundHandler, errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(fileUpload({ useTempFiles: true }));
 
-app.use(express.static(__dirname + '../client/build/'));
+app.use(express.static(path.join(__dirname, '../client/build/')));
 
 app.disable("x-powered-by");
 
